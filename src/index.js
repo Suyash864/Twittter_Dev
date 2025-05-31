f8aba749b67b9fac227927419a21316836d4402f
SUYASH <jsuyash864@gmail.com>
2025-05-31 20:22:11 +0530

    Added tweet service with hashtag processing

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ const express = require('express');
 const connect = require('./config/database');
 const app = express();
 
-const TweetRepository = require('./repository/tweet-repository');
-const Comment = require('./models/comment');
-
 app.listen(3000, async () => {
     console.log('Server Started');
     await connect();
     console.log('mongo db connected');
-});  
+});
