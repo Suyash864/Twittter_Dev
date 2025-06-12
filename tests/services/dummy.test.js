aad81975f072a2c660a4a36ab6417cfcb5a315c0
SUYASH <jsuyash864@gmail.com>
2025-06-12 11:24:56 +0530

    Added basic setup for jest

---
+++ tests/services/dummy.test.js
@@ -0,0 +1,8 @@
+import { JsonWebTokenError } from "jsonwebtoken";
+import { execute } from "../../src/services/dummy-service.js";
+
+test('result is true and return Learning JS', () => {
+    // IMPL oof test
+    const result = execute();
+    expect(result).toBe('LearningJS');
+})
